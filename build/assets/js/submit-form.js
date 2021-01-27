702edfda116bcd7588e6c0f2ba43c675ab7b11fd
Miroslav Petrov <miroslavpetrov.collaboration@gmail.com>
2021-01-27 14:55:45 +0300

    reviews

--- build/assets/js/submit-form.js
+++ build/assets/js/submit-form.js
@@ -1,30 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   window.Parsley.on('form:submit', function() {
-
     let Container = this.$element.closest('.js-submit-message')[0];
-    if (Container) {
-      form = Container.querySelector('form'),
-      modal = Container.querySelector('.form-section__form-modal');
-      
-      form.classList.add('form--hidden');
-      modal.classList.remove('form-modal--hidden');
-      
-      window.setTimeout(()=>{
-        form.classList.remove('form--hidden');
-        modal.classList.add('form-modal--hidden');
-        form.reset();
-      }, 3000);
-
-      return false;
-    }
     
-    Container = this.$element.closest('.blog__form-container')[0];
-
     if (Container) {
-      Container.classList.add('blog__form-container--success');
+      form = Container.querySelector('form');
+
+      Container.classList.add('js-success');
 
       window.setTimeout(()=>{
-        Container.classList.remove('blog__form-container--success');
+        Container.classList.remove('js-success');
+        form.reset();
       }, 3000);
 
       return false;
